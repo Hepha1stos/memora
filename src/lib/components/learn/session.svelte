fd5cccd192878e429a316aa358bdd6875a2be943
Hepha1stos <moritz.brunner@gmx.de>
2024-12-14 14:02:20 +0100

    Improved learningSession

--- src/lib/components/learn/session.svelte
+++ src/lib/components/learn/session.svelte
@@ -1,29 +1,41 @@
 <script lang="ts">
 	import { categoryStore } from '$lib/stores/categoryStore';
 	import { flashcardStore } from '$lib/stores/flashcardStore';
-  
-	import { Button, Heading } from 'flowbite-svelte';
+
+	import { Button, Heading, Alert } from 'flowbite-svelte';
 	import { get, type Writable } from 'svelte/store';
 	import SessionActive from './sessionActive.svelte';
 
 	export let isSessionOpen: Writable<boolean>;
 
 	let categories = get(categoryStore);
-	let start:boolean = false;
+	let start: boolean = false;
 	let countedFlashcards = 0;
+	let triggerAlertBack: boolean = false;
+	let triggerAlertClose: boolean = false;
 
-	let pickedCategoryId:number;
+	let pickedCategoryId: number | null = null;
+
+	function ChecktriggerAlertClose() {
+		if (start) {
+			// Eine aktive Session läuft
+			triggerAlertClose = true;
+		} else {
+			// Keine Session aktiv, schließe direkt
+			closeSession();
+		}
+	}
 
 	function closeSession() {
+		triggerAlertClose = false;
 		isSessionOpen.set(false);
 	}
 
-
-  function count(){
-    countedFlashcards = get(flashcardStore).filter((flashcard) => {
+	function count() {
+		countedFlashcards = get(flashcardStore).filter((flashcard) => {
 			return flashcard.category_id === pickedCategoryId;
 		}).length;
-  }
+	}
 </script>
 
 <div class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-60">
@@ -33,11 +45,11 @@
 			<SessionActive {pickedCategoryId} />
 		{:else}
 			<Heading tag="h4" class="mb-1 text-center text-lg font-semibold">Learning Session</Heading>
-			<form class="max-w-sm mx-auto">
+			<form class="mx-auto max-w-sm">
 				Choose a Category and click "Start" <br /><br />
 				<select
 					id="categories"
-					class="bg-gray-50 border border-gray-300 focus:outline-none focus:border-teal text-gray-900 text-sm rounded-lg block w-full p-2.5"
+					class="focus:border-teal block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:outline-none"
 					bind:value={pickedCategoryId}
 					on:change={count}
 				>
@@ -46,18 +58,96 @@
 						<option value={c.id}>{c.name}</option>
 					{/each}
 				</select>
-				<br>
-				You have {countedFlashcards}
-				{countedFlashcards === 1 ? 'Flashcard' : 'Flashcards'} to learn<br />
-				<Button type="button" class="w-full mt-4" on:click={() => { start = true }}>Start</Button>
+				<br />
+				{#if pickedCategoryId == null}
+					<!----->
+				{:else}
+					You have {countedFlashcards}
+					{countedFlashcards === 1 ? 'Flashcard' : 'Flashcards'} to learn<br />
+					<Button
+						type="button"
+						class="mt-4 w-full"
+						on:click={() => {
+							start = true;
+						}}>Start</Button
+					>
+				{/if}
 			</form>
 		{/if}
-		<div class="mt-auto w-full flex justify-center">
-			<Button type="button" color="alternative" class="w-1/3" on:click={closeSession}>Close</Button>
-      {#if start}
-      <Button type="button" color="alternative" class="w-1/3 ml-4" on:click={() => {start = false}}>Back</Button>
-      {/if}
+		<div class="mt-auto flex w-full justify-center">
+			<Button type="button" color="alternative" class="w-1/3" on:click={ChecktriggerAlertClose}
+				>Close</Button
+			>
+			{#if start}
+				<Button
+					type="button"
+					color="alternative"
+					class="ml-4 w-1/3"
+					on:click={() => {
+						triggerAlertBack = true;
+					}}>Back</Button
+				>
+			{/if}
 		</div>
+
+		<!-- Alert for pressing Back Button -->
+		{#if triggerAlertBack}
+			<div class="z-60 fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+				<div class="w-1/3 rounded-lg  p-6 text-center">
+					<Alert class="text-left">
+						<span class="font-medium">Attention!</span> The running Session will not be saved!
+						Continue?
+						<div class="mt-4 flex justify-between">
+							<Button
+								type="button"
+								color="green"
+								class="w-1/3"
+								on:click={() => {
+									start = false;
+									triggerAlertBack = false;
+								}}
+							>
+								Yes
+							</Button>
+							<Button
+								type="button"
+								color="red"
+								class="w-1/3"
+								on:click={() => {
+									triggerAlertBack = false;
+								}}
+							>
+								No
+							</Button>
+						</div>
+					</Alert>
+				</div>
+			</div>
+		{/if}
+
+    <!-- Alert for pressing Close Button -->
+		{#if triggerAlertClose}
+			<div class="z-60 fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+				<div class="w-1/3 rounded-lg p-6 text-center">
+					<Alert class="text-left">
+						<span class="font-medium">Attention!</span> The running Session will not be saved!
+						Continue?
+						<div class="mt-4 flex justify-between">
+							<Button type="button" color="green" class="w-1/3" on:click={closeSession}>Yes</Button>
+							<Button
+								type="button"
+								color="red"
+								class="w-1/3"
+								on:click={() => {
+									triggerAlertClose = false;
+								}}
+							>
+								No
+							</Button>
+						</div>
+					</Alert>
+				</div>
+			</div>
+		{/if}
 	</div>
 </div>
-
--- src/lib/components/learn/sessionActive.svelte
+++ src/lib/components/learn/sessionActive.svelte
@@ -7,8 +7,10 @@
   let flashcards = [];
   let cardIndex:number = 0;
   let answer:boolean = false;
+  let correctAnswers:number;
+  let wrongAnswers:number;
 
-
+  let sessionAnswers: { flashcardId: number; answered: boolean }[] = [];
 
   export let pickedCategoryId:number;
 
@@ -21,35 +23,66 @@
   //funcs
 
   function handleKnown(){
-    cardIndex += 1;
+    
     answer = false
+    sessionAnswers.push({flashcardId:flashcards[cardIndex].id, answered:true})
+    cardIndex += 1;
     // UPDATE flashcard
   }
 
   function handleNotKnown(){
-    cardIndex += 1;
+    
     answer = false;
+    sessionAnswers.push({flashcardId:flashcards[cardIndex].id, answered:false})
+    cardIndex += 1;
   }
- 
-</script>
 
+  function evaluate(){
+    correctAnswers =0;
+    wrongAnswers =0 ;
+
+    sessionAnswers.forEach((el) => {
+      el.answered ? (correctAnswers+=1):(wrongAnswers+=1)
+    })
+
+  }
+  if(cardIndex > flashcards.length){
+    evaluate()
+  }
+
+</script>
 
 {#if flashcards.length > 0}
    {#if cardIndex < flashcards.length}
+   <div class="border-4 hover:shadow-2xl ease-in-out duration-300  bg-slate-200 rounded-lg w-full h-1/6 text-center flex items-center justify-center">
     <p>{flashcards[cardIndex].question}</p>
-      {#if answer}
+  </div>
+  <div class="border-1 hover:shadow-2xl ease-in-out duration-300 mt-2 bg-slate-100 rounded-lg w-full h-1/6 text-center flex items-center justify-center">
+    <p></p>
+    {#if answer}
         <p>{flashcards[cardIndex].answer}</p>
       {/if}
+  </div>
+      
 
       {#if !answer}
-        <Button type="button" class="w-full" on:click={()=>{answer = true}}>Answer</Button>
+        <Button type="button" class="w-full mt-8" on:click={()=>{answer = true}}>Answer</Button>
       {:else}
         <Button type="button" color="green" class="w-full mt-4 mb-4" on:click={handleKnown}>Known</Button>
         <Button type="button" color="red" class="w-full" on:click={handleNotKnown}>Not Known</Button>
       {/if}
     {:else}
-    <p>Endegelende</p>
-    <Button type="button" color="green" class="w-1/2">End Session</Button> <!--todo-->
+    <p>Evaluation</p>
+    {evaluate()}
+    <div>
+      <div>
+        Correct answers: {correctAnswers}
+      </div>
+      <div>
+        Wrong answers: {wrongAnswers}
+      </div>
+    </div>
+    <Button type="button" color="green" class="w-1/2 mt-4">End Session</Button> <!--todo-->
    {/if}
 {:else}
    <p>Keine Flashcards gefunden</p>
