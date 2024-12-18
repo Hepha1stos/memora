15ea61ea22102597137f4227133cba4ac2b6ec41
Hepha1stos <moritz.brunner@gmx.de>
2024-12-18 15:38:16 +0100

    moved logo to static

--- src/lib/components/landing/header.svelte
+++ src/lib/components/landing/header.svelte
@@ -8,7 +8,7 @@
 
 
 <div class="flex flex-col content-center mt-[-2rem]">
-	<div class="flex justify-center"><img src="src/lib/assets/logo.png" alt="memora logo" /></div>
+	<div class="flex justify-center"><img src="/assets/logo.png" alt="memora logo" /></div>
 	<div class="mt-9 text-md text-center">
 		Welcome back {user?.name}! Ready to make today awesome with a fresh learning session?
 	</div>
